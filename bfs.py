1a792cb1b977c67978c6cac74454f677c727c82b
Nima Sarang <nimasarang@gmail.com>
2024-09-27 12:44:15 -0700

    add level  to BFS

--- bfs.py
+++ bfs.py
@@ -1,11 +1,12 @@
 from collections import deque
 
 def bfs(graph, start):
-    visited = set([start])
+    visited = {start: 0}
     queue = deque([start])
     while queue:
         node = queue.popleft()
+        level = visited[node]
         for neighbor in graph[node]:
             if neighbor not in visited:
-                visited.add(neighbor)
+                visited[neighbor] = level + 1
                 queue.append(neighbor)
