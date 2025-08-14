af41d34bc10bcf443a03fa002129af24e2577e7b
Nima Sarang <nimasarang@gmail.com>
2025-08-14 14:56:20 -0700

    add initial documentation for common data structures and utility functions

---
+++ __init__.py
@@ -0,0 +1,11 @@
+"""
+Common python data structures and utility functions.
+
+from queue import PriorityQueue
+from collections import deque
+
+- dict: __getitem__, __setitem__, del, pop (key, default)
+- Set: add, remove, discard (no error if not present), pop (random)
+- deque: append (right), appendleft, pop (right), popleft
+- PriorityQueue: put (priority, item), get
+"""
